83e7ffa7d3fc1f0a88c00ca100fd89e3cd867cd1
Sodney-Plusnet <sidneypinsent3@hotmail.co.uk>
2022-12-13 12:24:54 +0000

    4 - Additional changes after review

--- controllers/games.js
+++ controllers/games.js
@@ -1,6 +1,6 @@
 const { request, response } = require("../app")
 const reviews = require("../db/data/development-data/reviews")
-const {selectCategories, selectReviews} = require("../models/games")
+const {selectCategories, selectReviews, selectReviewId} = require("../models/games")
 
 exports.getCategories = (request, response, next) => {
 selectCategories().then((categories)=>{
@@ -14,6 +14,14 @@ response.status(200).send({reviews})
 
 }).catch(next)
 
+}
+
+exports.getReviewId = (request, response, next) =>{
+    // console.log(request.params, "request.params")
+    const { review_id } = request.params;
+    selectReviewId(review_id).then((review)=>{
+        response.status(200).send({review})
+    }).catch(next)
 }
 
 // catch here
